dee37c88baa2cc35a1d9e7d24dec5851df2df601
Jdiosa12 <Juanchom04@gmail.com>
2025-02-20 21:59:57 -0500

    Update ContadorDeVocales.java

--- ContadorDeVocales/src/main/java/ContadorDeVocales.java
+++ ContadorDeVocales/src/main/java/ContadorDeVocales.java
@@ -4,16 +4,16 @@ public class ContadorDeVocales {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar la palabra al usuario
+        
         System.out.print("Ingresa una palabra en minúsculas: ");
         String palabra = scanner.nextLine();
 
         int vocales = 0, consonantes = 0;
 
-        // Definir las vocales
+        
         String vocalesLista = "aeiou";
 
-        // Recorrer la palabra caracter por caracter
+        
         for (char c : palabra.toCharArray()) {
             if (vocalesLista.indexOf(c) != -1) {
                 vocales++;
@@ -22,7 +22,7 @@ public class ContadorDeVocales {
             }
         }
 
-        // Mostrar el resultado
+        
         System.out.println("Número de vocales: " + vocales);
         System.out.println("Número de consonantes: " + consonantes);
 
